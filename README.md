2d920f78dc3a3585e55b9df0b2aeda0b1d4a058e
Bob Farrell <git@bob.frl>
2022-06-06 21:02:38 +0100

    Implement Vincenty distance calculation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Geoprojection
 
-Toolkit for translating distances (kilometers) from co-ordinates (latitude, longitude) onto geographically-precise (within reason) projections.
+Toolkit for translating distances (kilometers) from co-ordinates (latitude, longitude) onto geographically-precise (within reason) projections and calculating distances between pairs of co-ordinates.
 
 ## Installation
 
@@ -21,11 +21,29 @@ Provide a center point (latitude, longitude) and a distance (kilometers) to calc
 The default number of points is `36`. This can be overridden with the `points` parameter to `Geoprojection::Ellipse.new`.
 
 ```ruby
-ellipse = Geoprojection::Ellipse.new(latitude: 50.8169, longitude: 0.1367, distance: 10, points: 36)
+ellipse = Geoprojection::Ellipse.new(latitude: 50.8169, longitude: 0.1367, distance: 10, points: 6)
 ellipse.points
+
+# => [[0.2788814046153052, 50.8169], [0.20779070230765262, 50.968049947019516], [0.0656092976923474, 50.968049947019516], [-0.0054814046153052465, 50.8169], [0.0656092976923473, 50.66575005298048], [0.20779070230765262, 50.66575005298048], [0.2788814046153052, 50.8169]]
 ```
 
 The actual returned points will be N+1, as the start and end point are identical to provide a complete polygon.
+
+### Distance
+
+Calculate the [Vincenty distance](https://en.wikipedia.org/wiki/Vincenty%27s_formulae) between two pairs of co-ordinates (latitude, longitude) in miles or kilometers:
+
+```ruby
+distance = Geoprojection::Distance.new(
+  { latitude: 50.8169, longitude: 0.1367 },
+  { latitude: 51.2341, longitude: 1.4231 }
+)
+
+distance.kilometers
+# => 148.2691075520691
+distance.miles
+# => 92.13012362873673
+```
 
 ## Contributing
 
--- lib/geoprojection/distance.rb
+++ lib/geoprojection/distance.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 module Geoprojection
-  WGS84_MAJOR_AXIS = 6_378_137.0
-  WGS84_MINOR_AXIS = 6_356_752.314245
-  FLATTENING = (WGS84_MAJOR_AXIS - WGS84_MINOR_AXIS) / WGS84_MAJOR_AXIS
-  PRECISION = 1e-12
-
   # Vincenty distance calculation for a pair of latitude/longitude co-ordinates.
   class Distance
+    # rubocop:disable Metrics/AbcSize
     def initialize(point1, point2)
       @latitude1 = degrees_to_radians(point1.fetch(:latitude))
       @longitude1 = degrees_to_radians(point1.fetch(:longitude))
       @latitude2 = degrees_to_radians(point2.fetch(:latitude))
       @longitude2 = degrees_to_radians(point2.fetch(:longitude))
+      @tan_geo_latitude1 = (1 - FLATTENING) * Math.tan(@longitude1)
+      @tan_geo_latitude2 = (1 - FLATTENING) * Math.tan(@longitude2)
+      @cos_geo_latitude1 = 1 / Math.sqrt(1 + (@tan_geo_latitude1**2))
+      @cos_geo_latitude2 = 1 / Math.sqrt(1 + (@tan_geo_latitude2**2))
+      @sin_geo_latitude1 = @tan_geo_latitude1 * @cos_geo_latitude1
+      @sin_geo_latitude2 = @tan_geo_latitude2 * @cos_geo_latitude2
     end
+    # rubocop:enable Metrics/AbcSize
 
     def miles
       meters * METERS_PER_MILE
@@ -33,10 +36,10 @@ module Geoprojection
       val * DEGREES_PER_RADIAN
     end
 
-    def distance(theta, sintheta, costheta, cos_sq_alpha, cos2thetam)
+    def distance(theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m)
       r_square = reduced_square(cos_sq_alpha)
       vertex_b = r_square / 1024 * (256 + (r_square * (-128 + (r_square * (74 - (47 * r_square))))))
-      WGS84_MINOR_AXIS * vertex_a(r_square) * (theta - delta_theta(vertex_b, sintheta, cos2thetam, costheta))
+      WGS84_MINOR_AXIS * vertex_a(r_square) * (theta - delta_theta(vertex_b, sin_theta, cos2_theta_m, cos_theta))
     end
 
     def reduced_square(cos_sq_alpha)
@@ -47,61 +50,80 @@ module Geoprojection
       1 + (r_square / 16_384 * (4096 + (r_square * (-768 + (r_square * (320 - (175 * r_square)))))))
     end
 
-    def sine_theta(lam)
-      Math.sqrt(((cos_geo_latitude2 * Math.sin(lam))**2) + (((cos_geo_latitude1 * sin_geo_latitude2) - (sin_geo_latitude1 * cos_geo_latitude2 * Math.cos(lam)))**2))
+    def sine_theta(lambda1)
+      Math.sqrt(
+        ((@cos_geo_latitude2 * Math.sin(lambda1))**2) +
+        (((@cos_geo_latitude1 * @sin_geo_latitude2) -
+        (@sin_geo_latitude1 * @cos_geo_latitude2 * Math.cos(lambda1)))**2)
+      )
     end
 
-    def delta_theta(vertex_b, sintheta, cos2thetam, costheta)
-      vertex_b * sintheta * (cos2thetam + (vertex_b / 4 * (costheta * (-1 + (2 * (cos2thetam**2)) - (vertex_b / 6 * cos2thetam * (-3 + (4 * (sintheta**2) * (-3 + (4 * (cos2thetam**2))))))))))
+    def delta_theta(vertex_b, sin_theta, cos2_theta_m, cos_theta)
+      vertex_b * sin_theta * (
+        delta_theta1(cos2_theta_m, vertex_b, cos_theta) +
+        delta_theta2(cos2_theta_m, vertex_b) +
+        delta_theta3(sin_theta, cos2_theta_m)
+      )
+    end
+
+    def delta_theta1(cos2_theta_m, vertex_b, cos_theta)
+      cos2_theta_m + ((vertex_b / 4 * cos_theta * -1))
+    end
+
+    def delta_theta2(cos2_theta_m, vertex_b)
+      (2 * (cos2_theta_m**2)) - ((vertex_b / 6 * cos2_theta_m * -3))
+    end
+
+    def delta_theta3(sin_theta, cos2_theta_m)
+      (4 * (sin_theta**2) * -3) + (4 * (cos2_theta_m**2))
     end
 
     def computed_variables
-      latitude_distance = @latitude2 - @latitude1
-      lam = latitude_distance
+      lambda1 = latitude_distance
 
-      precision = PRECISION + 1
+      precision = VINCENTY_PRECISION * 2
 
-      while precision > PRECISION
-        theta, sintheta, costheta, cos_sq_alpha, cos2thetam, lam, precision = iteration_variables(lam, latitude_distance)
+      while precision > VINCENTY_PRECISION
+        theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m, lambda1, precision = iteration_variables(lambda1)
       end
-      [theta, sintheta, costheta, cos_sq_alpha, cos2thetam]
+      [theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m]
     end
 
-    def iteration_variables(lam, latitude_distance)
-      sintheta = sine_theta(lam)
-      costheta = (sin_geo_latitude1 * sin_geo_latitude2) + (cos_geo_latitude1 * cos_geo_latitude2 * Math.cos(lam))
-      theta = Math.atan2(sintheta, costheta)
-      sinalpha = cos_geo_latitude1 * cos_geo_latitude2 * Math.sin(lam) / sintheta
-      cos_sq_alpha = 1 - (sinalpha**2)
-      cos2thetam = costheta - (2 * sin_geo_latitude1 * sin_geo_latitude2 / cos_sq_alpha)
-      c = (FLATTENING / 16) * cos_sq_alpha * (4 + (FLATTENING * (4 - (3 * cos_sq_alpha))))
-      lam2 = lam
-      lam = latitude_distance + ((1 - c) * FLATTENING * sinalpha * (theta + (c * sintheta * (cos2thetam + c + (costheta * (-1 + (2 * (cos2thetam**2))))))))
-      [theta, sintheta, costheta, cos_sq_alpha, cos2thetam, lam, (lam - lam2).abs]
+    def iteration_variables(lambda1)
+      sin_theta = sine_theta(lambda1)
+      cos_theta = calculated_cos_theta(lambda1)
+      theta = Math.atan2(sin_theta, cos_theta)
+      sin_alpha = @cos_geo_latitude1 * @cos_geo_latitude2 * Math.sin(lambda1) / sin_theta
+      cos_sq_alpha = 1 - (sin_alpha**2)
+      cos2_theta_m = calculated_cos2_theta_m(cos_theta, cos_sq_alpha)
+      lambda2 = iteration_lambda(sin_alpha, theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m)
+      [theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m, lambda2, (lambda2 - lambda1).abs]
     end
 
-    def tan_geo_latitude1
-      @tan_geo_latitude1 ||= (1 - FLATTENING) * Math.tan(@longitude1)
+    # rubocop:disable Metrics/ParameterLists
+    def iteration_lambda(sin_alpha, theta, sin_theta, cos_theta, cos_sq_alpha, cos2_theta_m)
+      c = calculated_c(cos_sq_alpha)
+      latitude_distance + (
+        (1 - c) * FLATTENING * sin_alpha *
+        (theta + (c * sin_theta * (cos2_theta_m + c + ((cos_theta * -1) + (2 * (cos2_theta_m**2))))))
+      )
+    end
+    # rubocop:enable Metrics/ParameterLists
+
+    def calculated_cos_theta(lambda_)
+      (@sin_geo_latitude1 * @sin_geo_latitude2) + (@cos_geo_latitude1 * @cos_geo_latitude2 * Math.cos(lambda_))
     end
 
-    def tan_geo_latitude2
-      @tan_geo_latitude2 ||= (1 - FLATTENING) * Math.tan(@longitude2)
+    def calculated_c(cos_sq_alpha)
+      (FLATTENING / 16) * cos_sq_alpha * (4 + (FLATTENING * (4 - (3 * cos_sq_alpha))))
     end
 
-    def cos_geo_latitude1
-      @cos_geo_latitude1 ||= 1 / Math.sqrt(1 + (tan_geo_latitude1**2))
+    def calculated_cos2_theta_m(cos_theta, cos_sq_alpha)
+      cos_theta - (2 * @sin_geo_latitude1 * @sin_geo_latitude2 / cos_sq_alpha)
     end
 
-    def cos_geo_latitude2
-      @cos_geo_latitude2 ||= 1 / Math.sqrt(1 + (tan_geo_latitude2**2))
-    end
-
-    def sin_geo_latitude1
-      @sin_geo_latitude1 ||= tan_geo_latitude1 * cos_geo_latitude1
-    end
-
-    def sin_geo_latitude2
-      @sin_geo_latitude2 ||= tan_geo_latitude2 * cos_geo_latitude2
+    def latitude_distance
+      @latitude_distance ||= @latitude2 - @latitude1
     end
   end
 end
